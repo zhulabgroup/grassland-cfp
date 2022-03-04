810b968eb3839257a866506efbe30d4f3a68ee21
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2022-03-04 11:58:22 -0800

    Change to clean and build

--- _build.sh
+++ _build.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
+Rscript -e "rmarkdown::clean_site(preview = FALSE)"
 Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::gitbook')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
-Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
-
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::pdf_book')"
+# Rscript -e "bookdown::render_book('index.Rmd', 'bookdown::epub_book')"
