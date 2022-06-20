3749c27d1cabb61601970e12344efdfc748e7226
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2022-06-20 14:38:15 -0700

    Add "--vanilla" to bypass loading default package(s)

--- _setup.sh
+++ _setup.sh
@@ -3,4 +3,4 @@
 set -v
 
 ln -s /data/ZHULAB/grassland data
-Rscript -e "update.packages(ask = FALSE, Ncpus = 4, repos = 'https://cloud.r-project.org/')"
+Rscript --vanilla -e "update.packages(ask = FALSE, Ncpus = 4, repos = 'https://cloud.r-project.org/')"
