0bd6ec686c036cca9b718cd5b3e87f8a2603b010
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2022-09-23 15:01:09 -0700

    Rename Rmd scripts

--- R/deploy_main.R
+++ R/deploy_main.R
@@ -1,10 +1,10 @@
 # render word docx and upload/update to google drive
 rmarkdown::render(
-  input = "../figures-main.Rmd",
+  input = "../figures_main.Rmd",
   output_format = "bookdown::word_document2",
   output_dir = "../tests/"
 )
 googledrive::drive_put(
-  media = "../tests/figures-main.docx",
+  media = "../tests/figures_main.docx",
   path = googledrive::as_id("1cEXTonrqpdGTY-FNnI7LsY4MBX9Amkip")
 )
