bdd3b710d20ea5f2f8935ec86ecb7b3320173de1
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2022-09-23 14:34:51 -0700

    Change tmp/ to tests/

--- R/_deploy.R
+++ R/_deploy.R
@@ -1,10 +1,10 @@
 # render word docx and upload/update to google drive
 rmarkdown::render(
-  input = "figures-main.Rmd",
+  input = "../figures-main.Rmd",
   output_format = "bookdown::word_document2",
-  output_dir = "tmp"
+  output_dir = "../tests"
 )
 googledrive::drive_put(
-  media = "tmp/figures-main.docx",
+  media = "../tests/figures-main.docx",
   path = googledrive::as_id("1cEXTonrqpdGTY-FNnI7LsY4MBX9Amkip")
 )
