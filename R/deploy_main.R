fe33c34cde36994f3abacffbc9911677d53ef9c1
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2022-09-24 06:05:34 -0700

    Rename Rmd files

--- R/deploy_main.R
+++ R/deploy_main.R
@@ -1,7 +1,7 @@
 # render word docx and upload/update to google drive
 rmarkdown::render(
-  knit_root_dir = "..", # from R/ to project dir
-  input = "../vignettes/main_figures.Rmd",
+  knit_root_dir = "..", # set to project dir
+  input = "../vignettes/main-figures.Rmd",
   output_format = "bookdown::word_document2",
   output_file = paste0(tempdir(), "/Main figures.docx")
 )
