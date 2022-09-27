2ae027eaa663b48b438d6e8828aa9c248351560d
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2022-09-27 13:57:01 -0700

    Move JRGCE from Rmd to R

---
+++ scripts/clean-jrgce.R
@@ -0,0 +1,70 @@
+# pin count table
+pin_tbl <- read_csv(paste0(.path$com_raw, "JRGCE/Pin count.csv"), 
+                    col_types = "iiccicc") %>%
+  select(year = YEAR, plot = ID, species = SPECIES, abund = TOTAL) %>%
+  mutate(species = if_else(
+    str_starts(species, "Acmispon americanus"),
+    "Acmispon americanus var.americanus",
+    species
+  )) %>%
+  mutate(species = if_else(
+    str_starts(species, "Crepis"),
+    "Crepis vesicaria ssp. taraxacifolia",
+    species
+  )) %>%
+  filter(!is.na(abund))
+
+# species table
+spp_tbl <- read_csv(paste0(.path$com_raw, "JRGCE/Species.csv"),
+                    col_types = "ccccccccccccccc") %>%
+  mutate(guild = str_c(case_when(
+    NATIVE_CODE == "I" ~ "E", # Invasive -> Exotic
+    NATIVE_CODE == "N" ~ "N", # Native
+    NATIVE_CODE == "U" ~ "U", # Unknown
+    is.na(NATIVE_CODE) ~ "U"
+  ), FXNGROUP_CODE)) %>%
+  select(species = `Species or unit name`, guild)
+
+# treatment table
+trt_tbl <- read_csv(paste0(.path$com_raw, "JRGCE/Treatment.csv"),
+                    col_types = "iiiiciiiiiiiiic") %>%
+  pull(id) %>%
+  expand_grid(year = 1998:2014, plot = .) %>% # full year-treatment tibble
+  full_join(read_csv("data/community/raw/JRGCE/Treatment.csv", col_types = "iiiiciiiiiiiiic") %>%
+              select(plot = id, starts_with("Heat_"), Precip, CO2, Nitrogen),
+            by = "plot"
+  ) %>%
+  mutate( # recode treatment
+    tmp = case_when(
+      year >= 1998 & year <= 2003 ~ as.logical(Heat_1998_2003 - 1),
+      year >= 2004 & year <= 2006 ~ as.logical(Heat_2004_2006 - 1),
+      year >= 2007 & year <= 2014 ~ as.logical(Heat_2007_2014 - 1)
+    ),
+    ppt = as.logical(Precip - 1),
+    co2 = as.logical(CO2 - 1),
+    ntg = as.logical(Nitrogen - 1)
+  ) %>%
+  mutate(
+    tmp = if_else(year == 1998, FALSE, tmp),
+    ppt = if_else(year == 1998, FALSE, ppt),
+    co2 = if_else(year == 1998, FALSE, co2),
+    ntg = if_else(year == 1998, FALSE, ntg)
+  ) %>%
+  mutate(
+    treat = str_c(
+      ifelse(tmp, "T", "_"),
+      ifelse(ppt, "P", "_"),
+      ifelse(co2, "C", "_"),
+      ifelse(ntg, "N", "_")
+    )
+  ) %>%
+  select(year, plot, treat) %>%
+  arrange(year, plot)
+
+# join all tables
+jrgce_data <- pin_tbl %>%
+  left_join(spp_tbl, by = "species") %>%
+  left_join(trt_tbl, by = c("year", "plot")) %>%
+  mutate(site = "jrgce", abund_type = "point_intercept") %>%
+  select(site, year, plot, treat, species, guild, abund, abund_type) %>%
+  arrange(year, plot, species)
