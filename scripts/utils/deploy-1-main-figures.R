c0baeca6c839ad3f99645414277012934840b8e2
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2022-10-03 17:21:54 -0700

    Rename files

--- scripts/utils/deploy-1-main-figures.R
+++ scripts/utils/deploy-1-main-figures.R
@@ -2,12 +2,12 @@
 # ok to run in console; background jobs set working dir to project dir
 Sys.setenv(RSTUDIO_PANDOC = "/usr/lib/rstudio-server/bin/pandoc")
 rmarkdown::render(
-  input = "02-supp-figures.Rmd",
+  input = "1-main-figures.Rmd",
   output_format = "bookdown::word_document2",
-  output_file = paste0(tempdir(), "/Supplementary figures.docx")
+  output_file = paste0(tempdir(), "/Main figures.docx")
   # knit_root_dir = ".." # set to project dir
 )
 googledrive::drive_put(
-  media = paste0(tempdir(), "/Supplementary figures.docx"),
+  media = paste0(tempdir(), "/Main figures.docx"),
   path = googledrive::as_id("1cEXTonrqpdGTY-FNnI7LsY4MBX9Amkip")
 )
--- scripts/utils/deploy-2-supp-figures.R
+++ scripts/utils/deploy-2-supp-figures.R
@@ -2,12 +2,12 @@
 # ok to run in console; background jobs set working dir to project dir
 Sys.setenv(RSTUDIO_PANDOC = "/usr/lib/rstudio-server/bin/pandoc")
 rmarkdown::render(
-  input = "01-main-figures.Rmd",
+  input = "2-supp-figures.Rmd",
   output_format = "bookdown::word_document2",
-  output_file = paste0(tempdir(), "/Main figures.docx")
+  output_file = paste0(tempdir(), "/Supplementary figures.docx")
   # knit_root_dir = ".." # set to project dir
 )
 googledrive::drive_put(
-  media = paste0(tempdir(), "/Main figures.docx"),
+  media = paste0(tempdir(), "/Supplementary figures.docx"),
   path = googledrive::as_id("1cEXTonrqpdGTY-FNnI7LsY4MBX9Amkip")
 )
