a173709a713fddeb962c471eb6603aa9a8e71a67
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2022-10-03 17:20:47 -0700

    Update file paths

--- scripts/utils/setup.R
+++ scripts/utils/setup.R
@@ -26,13 +26,13 @@ if (!dir.exists("data")) { # create symlink; final release needs to copy relevan
   com_exp = "data/community/all-experimental-data.rds",
   com_obs = "data/community/all-observational-data.rds",
   geo_cfp = "data/occurrence/hotspots/hotspots_2016_1.shp",
-  geo_clim = "data/climate/climate-gbif-2022-09-18.rds",
+  geo_clim = "data/climate/all/climate-gbif-2022-10-03.rds",
   geo_grass = "data/occurrence/grassland/",
   geo_site = "data/community/site-info.rds",
   occ_bien = "data/occurrence/bien/bien-cfp-2022-04-27.rds",
-  occ_cch = "data/occurrence/cch/cch-cfp-2022-07-12.rds",
-  occ_gbif = "data/occurrence/gbif/gbif-consolidated-2022-09-18.rds",
-  occ_inat = "data/occurrence/inat/inat-cfp-2022-09-18.rds",
-  sum_niche = "data/occurrence/niche-estimates-cfp-2022-09-18.rds",
+  occ_cch = "data/occurrence/cch/cch-cfp-2022-10-03.rds",
+  occ_gbif = "data/occurrence/gbif/gbif-consolidated-2022-10-03.rds",
+  occ_inat = "data/occurrence/inat/inat-cfp-2022-10-03.rds",
+  sum_niche = "data/occurrence/niche-estimates-cfp-2022-10-03.rds",
   sum_thin = "data/occurrence/niche-estimates-cfp-2022-09-18-thin.rds"
 )
