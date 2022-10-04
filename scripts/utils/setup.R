b914b1af8807ff26acd8d83b7ae8f9b5fe6f4b45
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2022-10-03 17:00:50 -0700

    Update niche summary file

--- scripts/utils/setup.R
+++ scripts/utils/setup.R
@@ -41,7 +41,7 @@ if (!dir.exists("data")) { # create symlink; final release needs to copy relevan
   occ_cch = "data/occurrence/cch/cch-cfp-2022-10-03.rds",
   occ_gbif = "data/occurrence/gbif/gbif-consolidated-2022-10-03.rds",
   occ_inat = "data/occurrence/inat/inat-cfp-2022-10-03.rds",
-  sum_niche = "data/occurrence/niche-estimates-cfp-2022-09-18.rds",
+  sum_niche = "data/occurrence/niche-estimates-cfp-2022-10-03.rds",
   sum_niche_fig = "figures/species-climate-niche-2022-10-03.pdf",
   sum_thin = "data/occurrence/niche-estimates-cfp-2022-09-18-thin.rds"
 )
