60b46e6e11ac2333ebdfd666681efb37d8ff9dce
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2022-10-21 07:19:12 -0700

    Update CHELSA download path

--- scripts/get-chelsa.R
+++ scripts/get-chelsa.R
@@ -2,7 +2,7 @@
 param_list <- c("bio1", "bio12", "vpd_max")
 for (param in param_list) {
   url <- str_c(
-    "https://os.zhdk.cloud.switch.ch/envicloud/chelsa/chelsa_V2/GLOBAL/climatologies/1981-2010/bio/CHELSA_",
+    "https://envicloud.wsl.ch/envicloud/chelsa/chelsa_V2/GLOBAL/climatologies/1981-2010/bio/CHELSA_",
     param,
     "_1981-2010_V.2.1.tif"
   )
