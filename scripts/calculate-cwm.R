e67f10a54571acb5a6358c9a9d2a851766abfb99
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2022-10-24 06:57:03 -0700

    Move calculate CWM from Rmd to R

---
+++ scripts/calculate-cwm.R
@@ -0,0 +1,29 @@
+# calculate CWM (community weighted mean, or sd, etc.) like CTI and CPI
+
+# load niche estimates.
+niche_tbl <- read_rds(.path$sum_niche)
+
+# define summarize CWM function
+sum_cwm <- function(.) {
+  # CTI, CTI_sd, CPI, CPI_sd, CVI, CVI_sd
+  summarize(.,
+    tmp_com_mean = sum(abund * tmp_occ_mean) / sum(abund),
+    tmp_com_var = sum(abund * tmp_occ_mean^2) / sum(abund) - tmp_com_mean^2,
+    ppt_com_mean = sum(abund * ppt_occ_mean) / sum(abund),
+    ppt_com_var = sum(abund * ppt_occ_mean^2) / sum(abund) - ppt_com_mean^2,
+    vpd_com_mean = sum(abund * vpd_occ_mean) / sum(abund),
+    vpd_com_var = sum(abund * vpd_occ_mean^2) / sum(abund) - vpd_com_mean^2
+  )
+}
+
+# experimental data and CWM
+exp_tbl <- read_rds(.path$com_exp) %>%
+  inner_join(niche_tbl, by = "species") %>%
+  group_by(site, year, plot, treat) %>% # w/ treatment for exp
+  sum_cwm()
+
+# observational data and CWM
+obs_tbl <- read_rds(.path$com_obs) %>%
+  inner_join(niche_tbl, by = "species") %>%
+  group_by(site, year, plot) %>% # w/o treatment for obs
+  sum_cwm()
