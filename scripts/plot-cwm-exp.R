6b17c36a239397c6948f6ddfea9655395fcda22f
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2022-10-27 07:43:50 -0700

    Move and organize JRGCE CWM plots from Rmd to R

---
+++ scripts/plot-cwm-exp.R
@@ -0,0 +1,75 @@
+# plot CWM (community weighted mean, or sd, etc.) like CTI and CPI
+# define function to plot CWM for experimental data
+plot_cwm_exp <- function(data, cwm_lab, trt_lab, trt_col) {
+  ggplot(data, aes(x = year, y = com_idx, col = treat, group = interaction(treat, year))) +
+    geom_boxplot() +
+    ggpubr::stat_compare_means(
+      aes(group = treat),
+      method = "wilcox.test",
+      label = "p.signif", hide.ns = FALSE
+    ) +
+    labs(
+      x = "Year", y = cwm_lab,
+      color = trt_lab
+    ) +
+    scale_color_manual(values = c("black", trt_col))
+}
+
+plot_cwm_jrgce <- function(cwm_tag = "mean_cti", trt_tag = "tmp_all") {
+  # wrapper function to plot CWM in JRGCE
+  # filter JRGCE data
+  jrgce_tbl <- exp_tbl %>%
+    filter(site == "jrgce")
+
+  # filter data by treatment tag
+  if (trt_tag == "tmp_only") { # only ambient vs. elevated tmp treat
+    jrgce_tbl <- jrgce_tbl %>%
+      filter(treat %in% c("____", "T___"))
+  } else if (trt_tag == "tmp_all") { # all tmp treat
+    jrgce_tbl <- jrgce_tbl %>%
+      mutate(treat = str_sub(treat, start = 1L, end = 1L))
+  } else if (trt_tag == "ppt_only") { # only ambient vs. elevated ppt treat
+    jrgce_tbl <- jrgce_tbl %>%
+      filter(treat %in% c("____", "_P__"))
+  } else if (trt_tag == "ppt_all") { # all ppt treat
+    jrgce_tbl <- jrgce_tbl %>%
+      mutate(treat = str_sub(treat, start = 2L, end = 2L))
+  } else {
+    stop("Unknown treatment tag")
+  }
+
+  # filter data by response (CWM) tag
+  jrgce_tbl <- jrgce_tbl %>%
+    select(year, treat,
+      com_idx = switch(cwm_tag,
+        "mean_cti" = "tmp_com_mean",
+        "sd_cti" = "tmp_com_sd",
+        "mean_cpi" = "ppt_com_mean",
+        "sd_cpi" = "ppt_com_sd",
+        "mean_cvi" = "vpd_com_mean",
+        "sd_cvi" = "vpd_com_sd"
+      )
+    )
+
+  # make plot
+  plot_cwm_exp(jrgce_tbl,
+    cwm_lab = switch(cwm_tag,
+      "mean_cti" = "Mean CTI (°C)",
+      "sd_cti" = "Std dev CTI (°C)",
+      "mean_cpi" = "Mean CPI (mm)",
+      "sd_cpi" = "Std dev CPI (mm)",
+      "mean_cvi" = "Mean CVI (Pa)",
+      "sd_cvi" = "Std dev VPD (Pa)"
+    ),
+    trt_lab = str_sub(trt_tag, 1L, 3L) %>%
+      switch(
+        "tmp" = "Warming treatment",
+        "ppt" = "Watering treatment"
+      ),
+    trt_col = str_sub(trt_tag, 1L, 3L) %>%
+      switch(
+        "tmp" = "red",
+        "ppt" = "blue"
+      )
+  )
+}
