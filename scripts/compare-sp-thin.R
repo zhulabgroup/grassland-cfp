4bbe62114a0f1822039129a00d2957e085473e15
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2022-11-07 11:12:21 -0800

    Change thinned plot from scatter plot matrix to two scatter plots; update text

--- scripts/compare-sp-thin.R
+++ scripts/compare-sp-thin.R
@@ -6,24 +6,36 @@ niche_tbl <- read_rds(.path$sum_niche) %>%
 thin_tbl <- read_rds(.path$sum_thin) %>%
   inner_join(niche_tbl, ., by = "species")
 
-thin_tmp_gg <- thin_tbl %>%
-  select(
-    tmp_mean_full, tmp_mean_thin,
-    tmp_median_full, tmp_median_thin
-  ) %>%
-  GGally::ggpairs(
-    lower = list(continuous = GGally::wrap("points", alpha = 0.2)),
-    title = "Species temperature niche (°C)",
-    columnLabels = c("Full mean", "Thinned mean", "Full median", "Thinned median")
+thin_tmp_gg <-
+  ggplot(thin_tbl, aes(full_median_tmp, thin_median_tmp)) +
+  geom_point(alpha = .5) +
+  geom_smooth(method = "lm", formula = y ~ x - 1, se = FALSE, color = "red", lty = "dashed") +
+  ggpubr::stat_cor(
+    aes(label = paste(..r.label.., ..p.label.., sep = "*`,`~")),
+    p.accuracy = 0.05,
+    color = "red"
+  ) +
+  geom_abline(slope = 1, intercept = 0, color = "red") +
+  labs(x = "Full data median temperature (°C)", y = "Thinned data median temperature (°C)") +
+  theme(
+    aspect.ratio = 1,
+    strip.background = element_blank(),
+    strip.text.x = element_blank()
   )
 
-thin_ppt_gg <- thin_tbl %>%
-  select(
-    ppt_mean_full, ppt_mean_thin,
-    ppt_median_full, ppt_median_thin
-  ) %>%
-  GGally::ggpairs(
-    lower = list(continuous = GGally::wrap("points", alpha = 0.2)),
-    title = "Species precipitation niche (mm)",
-    columnLabels = c("Full mean", "Thinned mean", "Full median", "Thinned median")
+thin_ppt_gg <-
+  ggplot(thin_tbl, aes(full_median_ppt, thin_median_ppt)) +
+  geom_point(alpha = .5) +
+  geom_smooth(method = "lm", formula = y ~ x - 1, se = FALSE, color = "red", lty = "dashed") +
+  ggpubr::stat_cor(
+    aes(label = paste(..r.label.., ..p.label.., sep = "*`,`~")),
+    p.accuracy = 0.05,
+    color = "red"
+  ) +
+  geom_abline(slope = 1, intercept = 0, color = "red") +
+  labs(x = "Full data median precipitation (mm)", y = "Thinned data median precipitation (mm)") +
+  theme(
+    aspect.ratio = 1,
+    strip.background = element_blank(),
+    strip.text.x = element_blank()
   )
