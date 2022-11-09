3abe1a8499a249010637d37ee624e4c95d5ec929
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2022-11-09 06:29:23 -0800

    Extract all bioclim variables

--- scripts/compare-bioclim.R
+++ scripts/compare-bioclim.R
@@ -1,78 +1,43 @@
-# read GBIF data in both CRS
-gbif_sf_wgs84 <- read_rds(.path$occ_gbif) %>%
-  mutate(
-    species = consolidatedName,
-    key = as.character(key)
-  ) %>%
-  dplyr::select(key, species, longitude, latitude) %>%
+# compare CHELSA bioclim variables (https://www.worldclim.org/data/bioclim.html)
+# read GBIF data
+gbif_sf <- read_rds(.path$occ_gbif) %>%
+  select(longitude, latitude) %>%
   distinct() %>%
   st_as_sf(
     coords = c("longitude", "latitude"),
     crs = "+proj=longlat +datum=WGS84 +no_defs"
   )
 
-gbif_sf_nad83 <- gbif_sf_wgs84 %>%
-  st_transform(crs = "+proj=longlat +datum=NAD83 +no_defs")
+# load all climate rasters
+bioclim_ras <- .path$cli_chelsa %>%
+  list.files(full.names = TRUE) %>%
+  raster::stack()
 
-
-# load all climate raster files
-
-# temp
-temp_ras <- raster::stack(
-  str_c(.path$cli_chelsa, "bio1.tif"),
-  str_c(.path$cli_chelsa, "bio5.tif"),
-  str_c(.path$cli_chelsa, "bio6.tif"),
-  str_c(.path$cli_chelsa, "bio8.tif"),
-  str_c(.path$cli_chelsa, "bio9.tif"),
-  str_c(.path$cli_chelsa, "bio10.tif"),
-  str_c(.path$cli_chelsa, "bio11.tif")
-)
-names(temp_ras) <- c("bio1", "bio5", "bio6", "bio8", "bio9", "bio10", "bio11")
-
-# extract climate data on GBIF locations
-temp_gbif_sf <- temp_ras %>%
-  raster::extract(gbif_sf_wgs84) %>%
+# extract values at GBIF occurrences
+bioclim_sf <- bioclim_ras %>%
+  raster::extract(gbif_sf) %>%
   as_tibble() %>%
-  bind_cols(gbif_sf_wgs84)
+  bind_cols(gbif_sf, .)
 
-library("PerformanceAnalytics")
-chart.Correlation(temp_gbif_sf[, 1:7] %>% sample_n(1000), histogram = TRUE, pch = 19)
+write_rds(bioclim_sf, .path$cli_bioclim_gbif)
 
+# temperature variables
+bioclim_sf %>%
+  st_drop_geometry() %>%
+  select(str_c("bio", c(1, 5, 6, 8:11))) %>%
+  sample_n(1e3) %>%
+  PerformanceAnalytics::chart.Correlation(histogram = TRUE, pch = 19)
 
-# prcp
-prcp_ras <- raster::stack(
-  str_c(.path$cli_chelsa, "bio12.tif"),
-  str_c(.path$cli_chelsa, "bio13.tif"),
-  str_c(.path$cli_chelsa, "bio14.tif"),
-  str_c(.path$cli_chelsa, "bio16.tif"),
-  str_c(.path$cli_chelsa, "bio17.tif"),
-  str_c(.path$cli_chelsa, "bio18.tif"),
-  str_c(.path$cli_chelsa, "bio19.tif")
-)
-names(prcp_ras) <- c("bio12", "bio13", "bio14", "bio16", "bio17", "bio18", "bio19")
+# precipitation variables
+bioclim_sf %>%
+  st_drop_geometry() %>%
+  select(str_c("bio", c(12:14, 16:19))) %>%
+  sample_n(1e3) %>%
+  PerformanceAnalytics::chart.Correlation(histogram = TRUE, pch = 19)
 
-# extract climate data on GBIF locations
-prcp_gbif_sf <- prcp_ras %>%
-  raster::extract(gbif_sf_wgs84) %>%
-  as_tibble() %>%
-  bind_cols(gbif_sf_wgs84)
-
-chart.Correlation(prcp_gbif_sf[, 1:7] %>% sample_n(1000), histogram = TRUE, pch = 19)
-
-# vpd
-vpd_ras <- raster::stack(
-  str_c(.path$cli_chelsa, "vpd_max.tif"),
-  str_c(.path$cli_chelsa, "vpd_mean.tif"),
-  str_c(.path$cli_chelsa, "vpd_min.tif")
-)
-names(vpd_ras) <- c("vpd_max", "vpd_mean", "vpd_min")
-
-# extract climate data on GBIF locations
-vpd_gbif_sf <- vpd_ras %>%
-  raster::extract(gbif_sf_wgs84) %>%
-  as_tibble() %>%
-  bind_cols(gbif_sf_wgs84)
-
-chart.Correlation(vpd_gbif_sf[, 1:3] %>% sample_n(1000), histogram = TRUE, pch = 19)
-
-# vpd_max does not correlate well with the vpd_min but bio1 and bio12 seem okay
+# vpd variables
+bioclim_sf %>%
+  st_drop_geometry() %>%
+  select(str_c("vpd_", c("max", "mean", "min"))) %>%
+  sample_n(1e3) %>%
+  PerformanceAnalytics::chart.Correlation(histogram = TRUE, pch = 19)
--- scripts/utils/setup.R
+++ scripts/utils/setup.R
@@ -25,6 +25,7 @@ if (!dir.exists("data")) { # create symlink; final release needs to copy relevan
 # - `sum`: summary data, e.g., niche estimates
 .path <- list( # hidden variable won't be removed
   cli_all_gbif = "data/climate/all/climate-gbif-2022-11-01.rds",
+  cli_bioclim_gbif = "data/climate/all/bioclim-gbif-2022-11-09.rds",
   cli_chelsa = "data/climate/chelsa/2022-11-09/",
   cli_chelsa_monthly = "data/climate/chelsa_monthly/",
   cli_prism = "data/climate/prism/2022-10-03/",
