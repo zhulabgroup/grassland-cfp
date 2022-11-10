3cfa155562ff630685a8d00065c4d41c92c51589
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2022-11-10 06:44:59 -0800

    Write function wrapper to plot site climate change

--- scripts/plot-site-climate.R
+++ scripts/plot-site-climate.R
@@ -1,41 +1,27 @@
 # explore annual site climate change patterns
-clim_tbl <- read_rds(.path$cli_chelsa_annual)
+plot_clim <- function(data, mapping, xlab = "Year", ylab) {
+  # define function to plot climate change ~ year | site
+  ggplot(data, mapping) +
+    geom_point() +
+    geom_smooth(
+      method = "lm", formula = y ~ x, se = FALSE,
+      color = "red", lty = "dashed"
+    ) +
+    ggpubr::stat_cor(aes(label = ..p.label..),
+      p.accuracy = 0.05,
+      color = "red"
+    ) +
+    facet_wrap(~name, scales = "free_y") +
+    labs(x = xlab, y = ylab)
+}
 
-tmp_gg <- ggplot(clim_tbl, aes(year, tmp)) +
-  geom_point() +
-  geom_smooth(
-    method = "lm", formula = y ~ x, se = FALSE,
-    color = "red", lty = "dashed"
-  ) +
-  ggpubr::stat_cor(aes(label = ..p.label..),
-    p.accuracy = 0.05,
-    color = "red"
-  ) +
-  facet_wrap(~name, scales = "free_y") +
-  labs(x = "Year", y = "Mean annual temperature (°C)")
+clim_tbl <- read_rds(.path$cli_chelsa_annual) %>%
+  filter(abbr %in% c(
+    "angelo", "carrizo", "elkhorn", "jasper", "mclann", "mclserp",
+    "morganterritory", "pleasantonridge", "sunol",
+    "swanton", "ucsc", "vascocaves"
+  ))
 
-ppt_gg <- ggplot(clim_tbl, aes(year, ppt)) +
-  geom_point() +
-  geom_smooth(
-    method = "lm", formula = y ~ x, se = FALSE,
-    color = "red", lty = "dashed"
-  ) +
-  ggpubr::stat_cor(aes(label = ..p.label..),
-    p.accuracy = 0.05,
-    color = "red"
-  ) +
-  facet_wrap(~name, scales = "free_y") +
-  labs(x = "Year", y = "Mean annual precipitation (mm)")
-
-vpd_gg <- ggplot(clim_tbl, aes(year, vpd)) +
-  geom_point() +
-  geom_smooth(
-    method = "lm", formula = y ~ x, se = FALSE,
-    color = "red", lty = "dashed"
-  ) +
-  ggpubr::stat_cor(aes(label = ..p.label..),
-    p.accuracy = 0.05,
-    color = "red"
-  ) +
-  facet_wrap(~name, scales = "free_y") +
-  labs(x = "Year", y = "Mean VPDmax (hPa)")
+tmp_gg <- plot_clim(clim_tbl, aes(x = year, y = tmp), ylab = "Mean annual temperature (°C)")
+ppt_gg <- plot_clim(clim_tbl, aes(x = year, y = ppt), ylab = "Mean annual precipitation (mm)")
+vpd_gg <- plot_clim(clim_tbl, aes(x = year, y = tmp), ylab = "Mean VPDmax (hPa)")
