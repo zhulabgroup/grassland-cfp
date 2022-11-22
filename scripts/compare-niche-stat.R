7908940c066e91a67e57f7abb0654a8aabd91c11
Kai Zhu <kai.zhu@socs-stats.ucsc.edu>
2022-11-22 12:33:35 -0800

    Update niche stats figure: remove title and switch axis labels (x bottom, y left)

--- scripts/compare-niche-stat.R
+++ scripts/compare-niche-stat.R
@@ -3,21 +3,31 @@ niche_tbl <- read_rds(.path$sum_niche) %>%
   filter(occ_n > 100) # no dummy species
 
 # temperature niche stats
-niche_tmp_gg <- niche_tbl %>%
+niche_tmp_gg <-
+  niche_tbl %>%
   dplyr::select(tmp_occ_mean, tmp_occ_median, tmp_occ_q05, tmp_occ_q95) %>%
   GGally::ggpairs(
     lower = list(continuous = GGally::wrap("points", alpha = 0.2)),
-    title = "Species temperature niche (°C)",
-    columnLabels = c("Mean", "Median", "Lower limit (5%)", "Upper limit (95%)")
+    # title = "Species temperature niche (°C)",
+    columnLabels = c("Mean (°C)", "Median (°C)", "Lower limit (5%, °C)", "Upper limit (95%, °C)"),
+    switch = "both"
   ) +
-  theme(strip.background = element_blank())
+  theme(
+    strip.background = element_blank(),
+    strip.placement = "outside"
+  )
 
 # precipitation niche stats
-niche_ppt_gg <- niche_tbl %>%
+niche_ppt_gg <-
+  niche_tbl %>%
   dplyr::select(ppt_occ_mean, ppt_occ_median, ppt_occ_q05, ppt_occ_q95) %>%
   GGally::ggpairs(
     lower = list(continuous = GGally::wrap("points", alpha = 0.2)),
-    title = "Species precipitation niche (mm)",
-    columnLabels = c("Mean", "Median", "Lower limit (5%)", "Upper limit (95%)")
+    # title = "Species precipitation niche (mm)",
+    columnLabels = c("Mean (mm)", "Median (mm)", "Lower limit (5%, mm)", "Upper limit (95%, mm)"),
+    switch = "both"
   ) +
-  theme(strip.background = element_blank())
+  theme(
+    strip.background = element_blank(),
+    strip.placement = "outside"
+  )
