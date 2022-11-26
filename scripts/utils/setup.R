7ae7213a79a874c7c7f1b26604a9c4fe66c05968
Kai Zhu <kai.zhu@ENVS-kaizhu-MacBook.local>
2022-11-25 21:43:52 -0500

    Update Turbo drive path again

--- scripts/utils/setup.R
+++ scripts/utils/setup.R
@@ -14,7 +14,7 @@ knitr::opts_chunk$set(
 
 # link paths
 if (!dir.exists("data")) { # create symlink; final release needs to copy relevant files
-  system("ln -s /data/ZHULAB/grassland data")
+  system("ln -s ~/.turbo/grassland data")
 }
 
 # data path tags are
