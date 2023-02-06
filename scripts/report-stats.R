0b2192fda7609807932cff3671bb246935fe9ac8
Kai Zhu <zhukai@umich.edu>
2023-02-06 11:21:13 -0500

    Move JRGCE temperatures chuck back

--- scripts/report-stats.R
+++ scripts/report-stats.R
@@ -113,6 +113,16 @@ exp_cpi_tbl %>%
   mutate(across(Ambient:Diff, signif, 3)) %>%
   knitr::kable()
 
+# get JRGCE temperature treatments in deg C
+jrgce_avgt_tbl <- .path$com_raw %>%
+  str_c("JRGCE/Environment mid H1 H2-1_V2.CSV") %>%
+  read_csv(col_types = "icfcd") %>%
+  rename_with(tolower) %>%
+  filter(variable == "AVGT308_126") %>%
+  pivot_wider(names_from = treatment, values_from = value) %>%
+  rename_with(tolower) %>%
+  mutate(tmp_diff = t_elev - amb)
+
 # obs data
 obs_tbl <- read_rds(.path$com_obs) %>%
   inner_join(niche_tbl, by = "species") %>%
