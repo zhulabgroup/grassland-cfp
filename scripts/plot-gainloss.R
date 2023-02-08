1b8d828f8c7313b20b85b2faaf74cbbd033022bd
yiluansong <yiluansong@gmail.com>
2023-02-08 18:41:09 -0500

    gainloss

--- scripts/plot-gainloss.R
+++ scripts/plot-gainloss.R
@@ -23,18 +23,25 @@ read_rds(.path$com_obs) %>%
 
 obs_gainloss_tbl_list <- vector(mode = "list")
 for (siteoi in names(site_vec)) {
-  obs_gainloss_tbl_list[[siteoi]] <- read_rds(.path$com_obs) %>%
+  df_trend<-read_rds(.path$com_obs) %>%
     filter(site == siteoi) %>%
+    filter(guild!="DUMMY") %>% 
     group_by(year, plot, species) %>%
     summarise(abund = sum(abund)) %>% # doing this because of duplicated records. not the ideal solution.
     ungroup() %>%
-    spread(key = "species", value = "abund") %>%
+    left_join(group_by(.,year, plot) %>% 
+                summarise(total=sum(abund)) %>% 
+                ungroup(),
+              by=c("year", "plot")) %>% 
+    mutate(rel_abun=abund/total) %>%
+    select(-abund, -total) %>% 
+    spread(key = "species", value = "rel_abun") %>%
     mutate_if(is.numeric, ~ replace_na(., 0)) %>%
-    gather(key = "species", value = "abund", -year, -plot) %>%
+    gather(key = "species", value = "rel_abun", -year, -plot) %>%
     group_by(species) %>%
     nest() %>%
     mutate(
-      map(data, ~ lm(abund ~ year, data = .)) %>%
+      map(data, ~ lm(rel_abun ~ year, data = .)) %>%
         map_df(~ broom::tidy(.) %>%
           filter(term == "year") %>%
           select(estimate, p.value)),
@@ -45,7 +52,40 @@ for (siteoi in names(site_vec)) {
       (estimate > 0 & p.value <= 0.05) ~ "gain",
       (estimate < 0 & p.value <= 0.05) ~ "loss",
       TRUE ~ "no clear change"
-    )) %>%
+    )) 
+  
+  df_dominance<-read_rds(.path$com_obs) %>%
+    filter(site == siteoi) %>%
+    filter(guild!="DUMMY") %>% 
+    group_by(year, plot, species) %>%
+    summarise(abund = sum(abund)) %>% # doing this because of duplicated records. not the ideal solution.
+    ungroup() %>% 
+    group_by(species) %>% 
+    summarise(abund=sum(abund)) %>% 
+    ungroup() %>% 
+    mutate(dominance=abund/sum(abund)) %>% 
+    select(-abund)
+    
+  df_complete<-read_rds(.path$com_obs) %>%
+    filter(site == siteoi) %>%
+    filter(guild!="DUMMY") %>% 
+    mutate(period =case_when (year %in% (year %>% unique() %>% sort() %>% head(5))~"early",
+                              year %in% (year %>% unique() %>% sort()  %>% tail(5))~"late")) %>% 
+    filter(!is.na(period)) %>% 
+    group_by(period, species) %>%
+    summarise(abund = sum(abund)) %>% # doing this because of duplicated records. not the ideal solution.
+    ungroup() %>%
+    spread(key = "species", value = "abund") %>%
+    mutate_if(is.numeric, ~ replace_na(., 0)) %>%
+    gather(key = "species", value = "abund", -period) %>%
+    spread(key="period", value="abund") %>% 
+    mutate(complete=case_when((early==0 & late!=0 )~"recruited",
+                              (early!=0 & late==0 )~"extirpated")) %>% 
+    select(species, complete)
+  
+  obs_gainloss_tbl_list[[siteoi]] <- df_trend %>% 
+    left_join(df_dominance, by="species") %>% 
+    left_join(df_complete, by = "species") %>% 
     left_join(niche_tbl, by = "species") %>%
     mutate(site = siteoi)
 }
@@ -66,12 +106,22 @@ gainloss_gg <-
       x = tmp_occ_median,
       y = ppt_occ_median,
       color = change,
-      size = estimate %>% abs() %>% sqrt()
-    ), alpha = 0.5
+      size = dominance
+    ), alpha = 1,pch=21, fill=NA
+  ) +
+  geom_point(
+    data = obs_gainloss_tbl %>% filter(change=="gain"|change=="loss") %>% filter(!is.na(complete)),
+    aes(
+      x = tmp_occ_median,
+      y = ppt_occ_median,
+      size = dominance ,
+      fill = complete,
+    ), alpha = 0.75,pch=21
   ) +
   scale_color_manual(values = c(gain = "dark green", `no clear change` = "gray", loss = "dark orange")) +
+  scale_fill_manual(values = c(recruited = "dark green",  extirpated = "dark orange")) +
   labs(x = "Mean annual temperature (°C)", y = "Mean annual precipitation (mm)") +
-  guides(size = "none") +
+  guides(fill = "none") +
   facet_wrap(. ~ site,
     labeller = site_vec %>% as_labeller(),
     nrow = 3
