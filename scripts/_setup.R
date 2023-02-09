25d2d57f5098672a89c01418ebef737710278a35
yiluansong <yiluansong@gmail.com>
2023-02-08 22:08:15 -0500

    terraclim cwd

--- scripts/_setup.R
+++ scripts/_setup.R
@@ -27,14 +27,14 @@ if (!dir.exists("data")) { # create symlink; final release needs to copy relevan
 # - `occ`: occurrence data
 # - `sum`: summary data, e.g., niche estimates
 .path <- list( # hidden variable won't be removed
-  cli_all_gbif = "data/climate/all/climate-gbif-2022-11-01.rds",
+  cli_all_gbif = "data/climate/all/climate-gbif-2023-02-08.rds",
   cli_bioclim_gbif = "data/climate/all/bioclim-gbif-2022-11-09.rds",
   cli_chelsa = "data/climate/chelsa/2022-11-09/",
   cli_chelsa_annual = "data/climate/chelsa/chelsa-annual-2022-11-10.rds",
   cli_chelsa_monthly = "data/climate/chelsa/monthly/",
   cli_chelsa_cfp_annual = "data/climate/chelsa/cfp_annual/",
   cli_prism = "data/climate/prism/2022-10-03/",
-  cli_terraclimate = "data/climate/terraclimate/2022-10-03/",
+  cli_terraclimate = "data/climate/terraclimate/2023-02-08/",
   com_exp = "data/community/all-experimental-data.rds",
   com_obs = "data/community/all-observational-data.rds",
   com_raw = "data/community/raw/",
