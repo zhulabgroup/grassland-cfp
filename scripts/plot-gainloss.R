28aa84e877764ee760bb62b3377db40cec179737
yiluansong <yiluansong@gmail.com>
2023-03-08 18:46:24 -0500

    gainloss fig, summary, stat

--- scripts/plot-gainloss.R
+++ scripts/plot-gainloss.R
@@ -1,3 +1,260 @@
+### observational sites
+niche_tbl <- read_rds(.path$sum_niche) %>%
+  filter(occ_n > 100 | is.na(occ_n)) %>% # species with many observations and dummy species
+  select(species, tmp_occ_median, ppt_occ_median)
+
+site_vec <- c(
+  angelo = "Angelo Coast",
+  carrizo = "Carrizo Plain",
+  elkhorn = "Elkhorn Slough",
+  jasper = "Jasper Ridge Serpentine",
+  mclann = "McLaughlin Annual",
+  mclserp = "McLaughlin Serpentine",
+  morganterritory = "Morgan Territory",
+  pleasantonridge = "Pleasanton Ridge",
+  sunol = "Sunol",
+  swanton = "Swanton Ranch",
+  ucsc = "UC Santa Cruz",
+  vascocaves = "Vasco Caves"
+)
+
+if (FALSE) {
+  obs_gainloss_tbl_list <- vector(mode = "list")
+  for (siteoi in names(site_vec)) {
+    df_trend <- read_rds(.path$com_obs) %>%
+      filter(site == siteoi) %>%
+      filter(guild != "DUMMY") %>%
+      group_by(year, plot, species) %>%
+      summarise(abund = sum(abund)) %>% # doing this because of duplicated records. not the ideal solution.
+      ungroup() %>%
+      left_join(
+        group_by(., year, plot) %>%
+          summarise(total = sum(abund)) %>%
+          ungroup(),
+        by = c("year", "plot")
+      ) %>%
+      mutate(rel_abun = abund / total) %>%
+      select(year, plot, species, abund) %>%
+      spread(key = "species", value = "abund") %>%
+      mutate_if(is.numeric, ~ replace_na(., 0)) %>%
+      gather(key = "species", value = "abund", -year, -plot) %>%
+      group_by(species) %>%
+      nest() %>%
+      mutate(
+        map(data, ~ lm(abund ~ year, data = .)) %>%
+          map_df(~ broom::tidy(.) %>%
+            filter(term == "year") %>%
+            select(estimate, p.value)),
+      ) %>%
+      unnest(cols = data) %>%
+      distinct(species, estimate, p.value) %>%
+      mutate(change = case_when(
+        (estimate > 0 & p.value <= 0.05) ~ "gain",
+        (estimate < 0 & p.value <= 0.05) ~ "loss",
+        TRUE ~ "no clear change"
+      ))
+
+    df_dominance <- read_rds(.path$com_obs) %>%
+      filter(site == siteoi) %>%
+      filter(guild != "DUMMY") %>%
+      group_by(year, plot, species) %>%
+      summarise(abund = sum(abund)) %>% # doing this because of duplicated records. not the ideal solution.
+      ungroup() %>%
+      group_by(species) %>%
+      summarise(abund = sum(abund)) %>%
+      ungroup() %>%
+      mutate(dominance = abund / sum(abund)) %>%
+      select(-abund)
+
+    df_complete <- read_rds(.path$com_obs) %>%
+      filter(site == siteoi) %>%
+      filter(guild != "DUMMY") %>%
+      mutate(period = case_when(
+        year %in% (year %>% unique() %>% sort() %>% head(5)) ~ "early",
+        year %in% (year %>% unique() %>% sort() %>% tail(5)) ~ "late"
+      )) %>%
+      filter(!is.na(period)) %>%
+      group_by(period, species) %>%
+      summarise(abund = sum(abund)) %>% # doing this because of duplicated records. not the ideal solution.
+      ungroup() %>%
+      spread(key = "species", value = "abund") %>%
+      mutate_if(is.numeric, ~ replace_na(., 0)) %>%
+      gather(key = "species", value = "abund", -period) %>%
+      spread(key = "period", value = "abund") %>%
+      mutate(complete = case_when(
+        (early == 0 & late != 0) ~ "recruited",
+        (early != 0 & late == 0) ~ "extirpated"
+      )) %>%
+      select(species, complete)
+
+    obs_gainloss_tbl_list[[siteoi]] <- df_trend %>%
+      left_join(df_dominance, by = "species") %>%
+      left_join(df_complete, by = "species") %>%
+      left_join(niche_tbl, by = "species") %>%
+      mutate(site = siteoi)
+
+    print(siteoi)
+  }
+  obs_gainloss_tbl <- bind_rows(obs_gainloss_tbl_list)
+
+  write_rds(obs_gainloss_tbl, str_c(.path$sum_gainloss, "obs.rds"))
+}
+
+obs_gainloss_tbl <- read_rds(str_c(.path$sum_gainloss, "obs.rds"))
+
+obs_gainloss_tbl %>%
+  group_by(species, change) %>%
+  summarize(n = n()) %>%
+  filter(change != "no clear change") %>%
+  group_by(change) %>%
+  arrange(desc(n)) %>%
+  slice(1)
+
+obs_gainloss_tbl %>%
+  group_by(species, complete) %>%
+  summarize(n = n()) %>%
+  filter(!is.na(complete)) %>%
+  group_by(complete) %>%
+  arrange(desc(n)) %>%
+  slice(1)
+
+obs_gainloss_tbl_long <- obs_gainloss_tbl %>%
+  pivot_longer(cols = tmp_occ_median:ppt_occ_median, names_to = "variable", values_to = "value") %>%
+  mutate(variable = factor(variable,
+    levels = c("tmp_occ_median", "ppt_occ_median"),
+    labels = c("tmp", "ppt")
+  ))
+
+obs_gainloss_tbl_long %>%
+  group_by(variable, change) %>%
+  summarise(
+    median = median(value, na.rm = T),
+    lower = quantile(value, 0.025, na.rm = T),
+    upper = quantile(value, 0.975, na.rm = T)
+  )
+
+wilcox.test(obs_gainloss_tbl_long %>% filter(variable == "tmp", change == "gain") %>% pull(value),
+  obs_gainloss_tbl_long %>% filter(variable == "tmp", change == "loss") %>% pull(value),
+  alternative = "two.sided"
+)
+
+wilcox.test(obs_gainloss_tbl_long %>% filter(variable == "ppt", change == "gain") %>% pull(value),
+  obs_gainloss_tbl_long %>% filter(variable == "ppt", change == "loss") %>% pull(value),
+  alternative = "two.sided"
+)
+
+obs_gainloss_summ_gg <-
+  ggplot(obs_gainloss_tbl_long %>%
+    mutate(change = if_else(change == "no clear change", "no clear\nchange", change))) +
+  geom_boxplot(aes(x = change, col = change, y = value)) +
+  scale_color_manual(values = c(gain = "dark green", `no clear change` = "lightgray", loss = "dark orange")) +
+  facet_wrap(. ~ variable,
+    ncol = 1,
+    scales = "free_y",
+    strip.position = "left",
+    labeller = labeller(variable = c(
+      tmp = "Mean annual temperature (°C)",
+      ppt = "Mean annual precipitation (mm)"
+    ))
+  ) +
+  guides(col = "none") +
+  labs(
+    x = NULL,
+    y = NULL
+  ) +
+  theme(
+    strip.background = element_blank(),
+    strip.placement = "outside"
+  )
+
+obs_gainloss_main_gg <-
+  ggplot() +
+  geom_point(
+    data = niche_tbl,
+    aes(
+      x = tmp_occ_median,
+      y = ppt_occ_median
+    ), color = "gray", alpha = 0
+  ) +
+  geom_point(
+    data = obs_gainloss_tbl,
+    aes(
+      x = tmp_occ_median,
+      y = ppt_occ_median,
+      color = change,
+      size = dominance
+    ), alpha = 1, pch = 21, fill = NA
+  ) +
+  geom_point(
+    data = obs_gainloss_tbl %>% filter(change == "gain" | change == "loss") %>% filter(!is.na(complete)),
+    aes(
+      x = tmp_occ_median,
+      y = ppt_occ_median,
+      size = dominance,
+      fill = complete,
+    ), alpha = 0.75, pch = 21
+  ) +
+  scale_color_manual(values = c(gain = "dark green", `no clear change` = "lightgray", loss = "dark orange")) +
+  scale_fill_manual(values = c(recruited = "dark green", extirpated = "dark orange")) +
+  labs(x = "Mean annual temperature (°C)", y = "Mean annual precipitation (mm)") +
+  guides(
+    fill = "none",
+    size = "none",
+    color = "none"
+  )
+
+obs_gainloss_main_3row_gg <- obs_gainloss_main_gg +
+  facet_wrap(. ~ site,
+    labeller = site_vec %>% as_labeller(),
+    nrow = 3
+  )
+
+obs_gainloss_main_2row_gg <- obs_gainloss_main_gg +
+  facet_wrap(. ~ site,
+    labeller = site_vec %>% as_labeller(),
+    nrow = 2
+  )
+
+obs_gainloss_supp_gg <- obs_gainloss_main_gg +
+  ggrepel::geom_text_repel(
+    data = obs_gainloss_tbl %>% filter(change == "gain" | change == "loss") %>% filter(!is.na(complete)),
+    aes(
+      x = tmp_occ_median,
+      y = ppt_occ_median,
+      color = change,
+      label = paste0("italic('", species, "')")
+    ),
+    size = 3.88 / 1.68,
+    alpha = 1,
+    cex = 3,
+    max.overlaps = 100,
+    parse = T
+  )
+
+obs_gainloss_supp_3row_gg <-
+  obs_gainloss_supp_gg +
+  facet_wrap(. ~ site,
+    labeller = site_vec %>% as_labeller(),
+    nrow = 3
+  )
+
+obs_gainloss_supp_2row_gg <-
+  obs_gainloss_supp_gg +
+  facet_wrap(. ~ site,
+    labeller = site_vec %>% as_labeller(),
+    nrow = 2
+  )
+
+# save figure file
+if (.fig_save) {
+  ggsave(
+    plot = obs_gainloss_supp_3row_gg,
+    filename = str_c(.path$out_fig, "fig-supp-gainloss-obs.png"),
+    width = 10,
+    height = 8
+  )
+}
+
 ### experimental site
 niche_tbl <- read_rds(.path$sum_niche) %>%
   filter(occ_n > 100 | is.na(occ_n)) %>% # species with many observations and dummy species
@@ -107,6 +364,63 @@ if (FALSE) {
 
 exp_gainloss_tbl <- read_rds(str_c(.path$sum_gainloss, "exp.rds"))
 
+exp_gainloss_tbl %>%
+  group_by(species, change) %>%
+  summarize(n = n()) %>%
+  filter(change != "no clear change") %>%
+  group_by(change) %>%
+  arrange(desc(n)) %>%
+  slice(1)
+
+exp_gainloss_tbl_long <- exp_gainloss_tbl %>%
+  pivot_longer(cols = tmp_occ_median:ppt_occ_median, names_to = "variable", values_to = "value") %>%
+  mutate(variable = factor(variable,
+    levels = c("tmp_occ_median", "ppt_occ_median"),
+    labels = c("tmp", "ppt")
+  ))
+
+exp_gainloss_tbl_long %>%
+  group_by(variable, change) %>%
+  summarise(
+    median = median(value, na.rm = T),
+    lower = quantile(value, 0.025, na.rm = T),
+    upper = quantile(value, 0.975, na.rm = T)
+  )
+
+wilcox.test(exp_gainloss_tbl_long %>% filter(variable == "tmp", change == "gain") %>% pull(value),
+  exp_gainloss_tbl_long %>% filter(variable == "tmp", change == "loss") %>% pull(value),
+  alternative = "two.sided"
+)
+
+wilcox.test(exp_gainloss_tbl_long %>% filter(variable == "ppt", change == "gain") %>% pull(value),
+  exp_gainloss_tbl_long %>% filter(variable == "ppt", change == "loss") %>% pull(value),
+  alternative = "two.sided"
+)
+
+exp_gainloss_summ_gg <-
+  ggplot(exp_gainloss_tbl_long %>%
+    mutate(change = if_else(change == "no clear change", "no clear\nchange", change))) +
+  geom_boxplot(aes(x = change, col = change, y = value)) +
+  scale_color_manual(values = c(gain = "dark green", `no clear change` = "lightgray", loss = "dark orange")) +
+  facet_wrap(. ~ variable,
+    ncol = 1,
+    scales = "free_y",
+    strip.position = "left",
+    labeller = labeller(variable = c(
+      tmp = "Mean annual temperature (°C)",
+      ppt = "Mean annual precipitation (mm)"
+    ))
+  ) +
+  guides(col = "none") +
+  labs(
+    x = NULL,
+    y = NULL
+  ) +
+  theme(
+    strip.background = element_blank(),
+    strip.placement = "outside"
+  )
+
 exp_gainloss_main_gg <-
   ggplot() +
   geom_point(
@@ -203,206 +517,18 @@ if (.fig_save) {
   )
 }
 
-### observational sites
-niche_tbl <- read_rds(.path$sum_niche) %>%
-  filter(occ_n > 100 | is.na(occ_n)) %>% # species with many observations and dummy species
-  select(species, tmp_occ_median, ppt_occ_median)
-
-site_vec <- c(
-  angelo = "Angelo Coast",
-  carrizo = "Carrizo Plain",
-  elkhorn = "Elkhorn Slough",
-  jasper = "Jasper Ridge Serpentine",
-  mclann = "McLaughlin Annual",
-  mclserp = "McLaughlin Serpentine",
-  morganterritory = "Morgan Territory",
-  pleasantonridge = "Pleasanton Ridge",
-  sunol = "Sunol",
-  swanton = "Swanton Ranch",
-  ucsc = "UC Santa Cruz",
-  vascocaves = "Vasco Caves"
-)
-
-if (FALSE) {
-  obs_gainloss_tbl_list <- vector(mode = "list")
-  for (siteoi in names(site_vec)) {
-    df_trend <- read_rds(.path$com_obs) %>%
-      filter(site == siteoi) %>%
-      filter(guild != "DUMMY") %>%
-      group_by(year, plot, species) %>%
-      summarise(abund = sum(abund)) %>% # doing this because of duplicated records. not the ideal solution.
-      ungroup() %>%
-      left_join(
-        group_by(., year, plot) %>%
-          summarise(total = sum(abund)) %>%
-          ungroup(),
-        by = c("year", "plot")
-      ) %>%
-      mutate(rel_abun = abund / total) %>%
-      select(year, plot, species, abund) %>%
-      spread(key = "species", value = "abund") %>%
-      mutate_if(is.numeric, ~ replace_na(., 0)) %>%
-      gather(key = "species", value = "abund", -year, -plot) %>%
-      group_by(species) %>%
-      nest() %>%
-      mutate(
-        map(data, ~ lm(abund ~ year, data = .)) %>%
-          map_df(~ broom::tidy(.) %>%
-            filter(term == "year") %>%
-            select(estimate, p.value)),
-      ) %>%
-      unnest(cols = data) %>%
-      distinct(species, estimate, p.value) %>%
-      mutate(change = case_when(
-        (estimate > 0 & p.value <= 0.05) ~ "gain",
-        (estimate < 0 & p.value <= 0.05) ~ "loss",
-        TRUE ~ "no clear change"
-      ))
-
-    df_dominance <- read_rds(.path$com_obs) %>%
-      filter(site == siteoi) %>%
-      filter(guild != "DUMMY") %>%
-      group_by(year, plot, species) %>%
-      summarise(abund = sum(abund)) %>% # doing this because of duplicated records. not the ideal solution.
-      ungroup() %>%
-      group_by(species) %>%
-      summarise(abund = sum(abund)) %>%
-      ungroup() %>%
-      mutate(dominance = abund / sum(abund)) %>%
-      select(-abund)
-
-    df_complete <- read_rds(.path$com_obs) %>%
-      filter(site == siteoi) %>%
-      filter(guild != "DUMMY") %>%
-      mutate(period = case_when(
-        year %in% (year %>% unique() %>% sort() %>% head(5)) ~ "early",
-        year %in% (year %>% unique() %>% sort() %>% tail(5)) ~ "late"
-      )) %>%
-      filter(!is.na(period)) %>%
-      group_by(period, species) %>%
-      summarise(abund = sum(abund)) %>% # doing this because of duplicated records. not the ideal solution.
-      ungroup() %>%
-      spread(key = "species", value = "abund") %>%
-      mutate_if(is.numeric, ~ replace_na(., 0)) %>%
-      gather(key = "species", value = "abund", -period) %>%
-      spread(key = "period", value = "abund") %>%
-      mutate(complete = case_when(
-        (early == 0 & late != 0) ~ "recruited",
-        (early != 0 & late == 0) ~ "extirpated"
-      )) %>%
-      select(species, complete)
-
-    obs_gainloss_tbl_list[[siteoi]] <- df_trend %>%
-      left_join(df_dominance, by = "species") %>%
-      left_join(df_complete, by = "species") %>%
-      left_join(niche_tbl, by = "species") %>%
-      mutate(site = siteoi)
-
-    print(siteoi)
-  }
-  obs_gainloss_tbl <- bind_rows(obs_gainloss_tbl_list)
-
-  write_rds(obs_gainloss_tbl, str_c(.path$sum_gainloss, "obs.rds"))
-}
-
-obs_gainloss_tbl <- read_rds(str_c(.path$sum_gainloss, "obs.rds"))
-
-obs_gainloss_main_gg <-
-  ggplot() +
-  geom_point(
-    data = niche_tbl,
-    aes(
-      x = tmp_occ_median,
-      y = ppt_occ_median
-    ), color = "gray", alpha = 0
-  ) +
-  geom_point(
-    data = obs_gainloss_tbl,
-    aes(
-      x = tmp_occ_median,
-      y = ppt_occ_median,
-      color = change,
-      size = dominance
-    ), alpha = 1, pch = 21, fill = NA
-  ) +
-  geom_point(
-    data = obs_gainloss_tbl %>% filter(change == "gain" | change == "loss") %>% filter(!is.na(complete)),
-    aes(
-      x = tmp_occ_median,
-      y = ppt_occ_median,
-      size = dominance,
-      fill = complete,
-    ), alpha = 0.75, pch = 21
-  ) +
-  scale_color_manual(values = c(gain = "dark green", `no clear change` = "lightgray", loss = "dark orange")) +
-  scale_fill_manual(values = c(recruited = "dark green", extirpated = "dark orange")) +
-  labs(x = "Mean annual temperature (°C)", y = "Mean annual precipitation (mm)") +
-  guides(
-    fill = "none",
-    size = "none",
-    color = "none"
-  )
-
-obs_gainloss_main_3row_gg <- obs_gainloss_main_gg +
-  facet_wrap(. ~ site,
-    labeller = site_vec %>% as_labeller(),
-    nrow = 3
-  )
-
-obs_gainloss_main_2row_gg <- obs_gainloss_main_gg +
-  facet_wrap(. ~ site,
-    labeller = site_vec %>% as_labeller(),
-    nrow = 2
-  )
-
-obs_gainloss_supp_gg <- obs_gainloss_main_gg +
-  ggrepel::geom_text_repel(
-    data = obs_gainloss_tbl %>% filter(change == "gain" | change == "loss") %>% filter(!is.na(complete)),
-    aes(
-      x = tmp_occ_median,
-      y = ppt_occ_median,
-      color = change,
-      label = paste0("italic('", species, "')")
-    ),
-    size = 3.88 / 1.68,
-    alpha = 1,
-    cex = 3,
-    max.overlaps = 100,
-    parse = T
-  )
-
-obs_gainloss_supp_3row_gg <-
-  obs_gainloss_supp_gg +
-  facet_wrap(. ~ site,
-    labeller = site_vec %>% as_labeller(),
-    nrow = 3
-  )
-
-obs_gainloss_supp_2row_gg <-
-  obs_gainloss_supp_gg +
-  facet_wrap(. ~ site,
-    labeller = site_vec %>% as_labeller(),
-    nrow = 2
-  )
-
-# save figure file
-if (.fig_save) {
-  ggsave(
-    plot = obs_gainloss_supp_3row_gg,
-    filename = str_c(.path$out_fig, "fig-supp-gainloss-obs.png"),
-    width = 10,
-    height = 8
-  )
-}
+### combine
 
 gainloss_main_gg <-
   obs_gainloss_main_3row_gg +
+  obs_gainloss_summ_gg +
   exp_gainloss_main_gg +
+  exp_gainloss_summ_gg +
   plot_layout(design = "
-  A
-  A
-  A
-  B
+  AAAAB
+  AAAAB
+  AAAAD
+  CCCCD
 ") +
   plot_annotation(tag_levels = "A")
 
@@ -412,7 +538,7 @@ if (.fig_save) {
     plot = gainloss_main_gg,
     filename = str_c(.path$out_fig, "fig-main-gainloss.png"),
     width = 10,
-    height = 10
+    height = 11
   )
 }
 
