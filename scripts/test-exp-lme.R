65c3552f5b41d85bd69653f7c9b09d224bc8ec80
yiluansong <yiluansong@gmail.com>
2023-03-08 20:36:59 -0500

    all exp lme and guild permanova

--- scripts/test-exp-lme.R
+++ scripts/test-exp-lme.R
@@ -3,7 +3,7 @@
 niche_tbl <- read_rds(.path$sum_niche) %>%
   filter(occ_n > 100 | is.na(occ_n)) # species with many observations and dummy species
 
-# JRGCE data
+# experiment data
 jrgce_tbl <- read_rds(.path$com_exp) %>%
   filter(site == "jrgce", year >= 1999) %>%
   inner_join(niche_tbl, by = "species") %>%
@@ -21,29 +21,55 @@ jrgce_tbl <- read_rds(.path$com_exp) %>%
     labels = c("CTI", "CPI")
   ))
 
-# statistical tests pooled across years
-data_lme <- jrgce_tbl %>%
-  mutate(phase = case_when(
-    year <= 2002 ~ 1,
-    year >= 2010 ~ 3,
-    TRUE ~ 2
+mclexp_tbl <- read_rds(.path$com_exp) %>%
+  filter(site == "mclexp") %>%
+  inner_join(niche_tbl, by = "species") %>%
+  group_by(site, year, plot, treat) %>%
+  summarize(
+    tmp_com_mean = sum(abund * tmp_occ_median) / sum(abund),
+    ppt_com_mean = sum(abund * ppt_occ_median) / sum(abund)
+  ) %>%
+  separate(treat, c("treat", "soil"), sep = 2) %>%
+  pivot_longer(cols = tmp_com_mean:ppt_com_mean, names_to = "com_idx_name", values_to = "com_idx_value") %>%
+  mutate(com_idx_name = factor(com_idx_name,
+    levels = c("tmp_com_mean", "ppt_com_mean"),
+    labels = c("CTI", "CPI")
   ))
 
-exp_lme_stat <- data.frame(
-  treatment = character(0),
-  metric = character(0),
-  phase = numeric(0),
-  estimate = numeric(0),
-  p = numeric(0)
-)
-for (treatment in c("warm", "water")) {
-  for (metric in c("CTI", "CPI")) {
-    if (treatment == "warm") {
-      for (p in 1:3) {
+scide_tbl <- read_rds(.path$com_exp) %>%
+  filter(str_detect(site, "scide")) %>%
+  inner_join(niche_tbl, by = "species") %>%
+  group_by(site, year, plot, treat) %>%
+  summarize(
+    tmp_com_mean = sum(abund * tmp_occ_median) / sum(abund),
+    ppt_com_mean = sum(abund * ppt_occ_median) / sum(abund)
+  ) %>%
+  pivot_longer(cols = tmp_com_mean:ppt_com_mean, names_to = "com_idx_name", values_to = "com_idx_value") %>%
+  mutate(com_idx_name = factor(com_idx_name,
+    levels = c("tmp_com_mean", "ppt_com_mean"),
+    labels = c("CTI", "CPI")
+  ))
+
+# statistical tests pooled across years
+lme_calc <- function(exp, trt) {
+  exp_lme_stat <- data.frame(
+    metric = character(0),
+    estimate = numeric(0),
+    p_val = numeric(0)
+  )
+
+  if (exp == "scide") {
+    data_lme <- scide_tbl %>%
+      mutate(treat_D = case_when(
+        treat == "_" ~ "ambient",
+        treat == "D" ~ "drought"
+      )) %>%
+      mutate(treat_D = factor(treat_D, levels = c("ambient", "drought")))
+    for (metric in c("CTI", "CPI")) {
+      if (trt == "drought") {
         data_sub <- data_lme %>%
-          mutate(treat = treat_T) %>%
-          filter(com_idx_name == metric) %>%
-          filter(phase == p)
+          mutate(treat = treat_D) %>%
+          filter(com_idx_name == metric)
         m <- nlme::lme(com_idx_value ~ treat,
           random = ~ 1 | year,
           data = data_sub
@@ -52,46 +78,165 @@ for (treatment in c("warm", "water")) {
         exp_lme_stat <- exp_lme_stat %>%
           bind_rows(
             data.frame(
-              treatment = treatment,
               metric = metric,
-              phase = p,
               estimate = m_sum$tTable[2, 1],
-              p = m_sum$tTable[2, 5]
+              p_val = m_sum$tTable[2, 5]
             )
           )
       }
     }
-    if (treatment == "water") {
-      data_sub <- data_lme %>%
-        mutate(treat = treat_P) %>%
-        filter(com_idx_name == metric)
-      m <- nlme::lme(com_idx_value ~ treat,
-        random = ~ 1 | year,
-        data = data_sub
-      )
-      m_sum <- summary(m)
+  }
 
-      exp_lme_stat <- exp_lme_stat %>%
-        bind_rows(
-          data.frame(
-            treatment = treatment,
-            metric = metric,
-            phase = NA,
-            estimate = m_sum$tTable[2, 1],
-            p = m_sum$tTable[2, 5]
-          )
+  if (exp == "mclexp") {
+    data_lme <- mclexp_tbl %>%
+      mutate(treat_W = case_when(
+        treat == "_X" ~ "ambient",
+        treat == "WX" ~ "water"
+      )) %>%
+      mutate(treat_W = factor(treat_W, levels = c("ambient", "water"))) %>%
+      mutate(treat_D = case_when(
+        treat == "X_" ~ "ambient",
+        treat == "XD" ~ "drought"
+      )) %>%
+      mutate(treat_D = factor(treat_D, levels = c("ambient", "drought")))
+
+    for (metric in c("CTI", "CPI")) {
+      if (trt == "water") {
+        s_list <- c("S", "N")
+      }
+      if (trt == "drought") {
+        s_list <- c("S")
+      }
+      for (s in s_list) {
+        data_sub <- data_lme %>%
+          mutate(treat = treat_W) %>%
+          filter(com_idx_name == metric) %>%
+          filter(soil == s) %>%
+          select(-treat_W, -treat_D) %>%
+          drop_na()
+        m <- nlme::lme(com_idx_value ~ treat,
+          random = ~ 1 | year,
+          data = data_sub
         )
+        m_sum <- summary(m)
+        exp_lme_stat <- exp_lme_stat %>%
+          bind_rows(
+            data.frame(
+              Soil = s,
+              metric = metric,
+              estimate = m_sum$tTable[2, 1],
+              p_val = m_sum$tTable[2, 5]
+            ) %>%
+              mutate(Soil = case_when(
+                Soil == "S" ~ "Serpentine",
+                Soil == "N" ~ "Non-serpentine"
+              )) %>%
+              mutate(Soil = factor(Soil, levels = c("Serpentine", "Non-serpentine")))
+          ) %>%
+          select(Soil, everything())
+      }
     }
   }
+
+  if (exp == "jrgce") {
+    data_lme <- jrgce_tbl %>%
+      mutate(phase = case_when(
+        year <= 2002 ~ "I",
+        year >= 2010 ~ "III",
+        TRUE ~ "II"
+      ))
+    for (metric in c("CTI", "CPI")) {
+      if (trt == "warm") {
+        for (p in c("I", "II", "III")) {
+          data_sub <- data_lme %>%
+            mutate(treat = treat_T) %>%
+            filter(com_idx_name == metric) %>%
+            filter(phase == p)
+          m <- nlme::lme(com_idx_value ~ treat,
+            random = ~ 1 | year,
+            data = data_sub
+          )
+          m_sum <- summary(m)
+          exp_lme_stat <- exp_lme_stat %>%
+            bind_rows(
+              data.frame(
+                Phase = p,
+                metric = metric,
+                estimate = m_sum$tTable[2, 1],
+                p_val = m_sum$tTable[2, 5]
+              )
+            ) %>%
+            select(Phase, everything())
+        }
+      }
+      if (trt == "water") {
+        data_sub <- data_lme %>%
+          mutate(treat = treat_P) %>%
+          filter(com_idx_name == metric)
+        m <- nlme::lme(com_idx_value ~ treat,
+          random = ~ 1 | year,
+          data = data_sub
+        )
+        m_sum <- summary(m)
+
+        exp_lme_stat <- exp_lme_stat %>%
+          bind_rows(
+            data.frame(
+              metric = metric,
+              estimate = m_sum$tTable[2, 1],
+              p_val = m_sum$tTable[2, 5]
+            )
+          )
+      }
+    }
+  }
+
+  lme_tbl <-
+    exp_lme_stat %>%
+    mutate(sig = gtools::stars.pval(p_val)) %>%
+    mutate(sig = ifelse(sig != " ", sig, "ns")) %>%
+    rename(Metric = metric, Estimate = estimate, `p-value` = p_val, Significance = sig) %>%
+    mutate(
+      across(Estimate, signif, 3),
+      across(`p-value`, round, 4)
+    )
+
+  return(lme_tbl)
 }
 
-lme_tbl <-
-  exp_lme_stat %>%
-  mutate(sig = case_when(
-    p <= 0.05 ~ "sig",
-    TRUE ~ "ns"
-  )) %>%
+jrgce_warm_lme <- lme_calc(exp = "jrgce", trt = "warm")
+jrgce_water_lme <- lme_calc(exp = "jrgce", trt = "water") %>%
   mutate(
-    across(estimate, signif, 3),
-    across(p, round, 4)
-  )
+    Experiment = "Jasper Ridge Global Change Experiment",
+    Treatment = "Watering"
+  ) %>%
+  select(Experiment, Treatment, everything())
+
+mclexp_water_lme <- lme_calc(exp = "mclexp", trt = "water") %>%
+  mutate(
+    Experiment = "McLaughlin Water Experiment",
+    Treatment = "Watering"
+  ) %>%
+  select(Experiment, Treatment, Soil, everything())
+
+mclexp_drought_lme <- lme_calc(exp = "mclexp", trt = "drought") %>%
+  mutate(
+    Experiment = "McLaughlin Water Experiment",
+    Treatment = "Drought"
+  ) %>%
+  select(Experiment, Treatment, Soil, everything())
+
+scide_drought_lme <- lme_calc(exp = "scide", trt = "drought") %>%
+  mutate(
+    Experiment = "Santa Cruz International Drought Experiment",
+    Treatment = "Drought"
+  ) %>%
+  select(Experiment, Treatment, everything())
+
+extra_lme <- bind_rows(
+  jrgce_water_lme,
+  mclexp_water_lme,
+  mclexp_drought_lme,
+  scide_drought_lme
+) %>%
+  select(Experiment, Treatment, Soil, everything())
