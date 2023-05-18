360594ef4ef46a71dbc884773f809379af558f52
Kai Zhu <zhukai@umich.edu>
2023-05-18 12:16:29 -0400

    Add tidy Santa Cruz International Drought Experiment

--- R/tidy.R
+++ R/tidy.R
@@ -15,6 +15,7 @@ tidy_all <- function(
   # experimental sites
   tidy_jrgce(indir) %>% write_csv(str_c(outdir, "jrgce.csv"))
   tidy_mclexp(indir) %>% write_csv(str_c(outdir, "mclexp.csv"))
+  tidy_scide(indir) %>% write_csv(str_c(outdir, "scide.csv"))
 }
 
 # observation -------------------------------------------------------------
@@ -827,4 +828,50 @@ tidy_mclexp <- function(basedir) {
     arrange(year, plot, species)
 
   return(mclexp_tbl)
+}
+
+tidy_scide <- function(basedir) {
+  scide_tbl <- basedir %>%
+    str_c("SantaCruzIDE/IDESpeciesComp_LongFormat_2015_2021.csv") %>%
+    read_csv(col_types = c("ciicdicdcc")) %>%
+    mutate(
+      site = Site %>%
+        str_replace(" ", "") %>%
+        tolower() %>%
+        str_c("scide_", .),
+      year = Year,
+      plot = str_c(Plot, Block, sep = "-"),
+      treat = case_when(
+        Treatment == "Ambient" ~ "_",
+        Treatment == "Drought" ~ "D"
+      ),
+      species = Species,
+      guild = str_c(
+        case_when( # origin
+          Origin == "Native" ~ "N",
+          Origin == "Non-native" ~ "E", # exotic
+          is.na(Origin) ~ "U" # unknown
+        ),
+        case_when( # lifeform
+          Lifeform == "Annual" ~ "AU", # annual unknown
+          Lifeform == "Annual forb" ~ "AF",
+          Lifeform == "Annual grass" ~ "AG",
+          Lifeform == "Annual rush" ~ "AR",
+          Lifeform == "N-fixer" ~ "NF",
+          Lifeform == "Perennial forb" ~ "PF",
+          Lifeform == "Perennial grass" ~ "PG",
+          Lifeform == "Perennial sedge" ~ "PS",
+          Lifeform == "Rhizomatous forb" ~ "RF",
+          Lifeform == "Rhizomatous grass" ~ "RG",
+          Lifeform == "Rhizomatous rush" ~ "RR",
+          Lifeform %in% c("Shrub", "Shurb") ~ "S",
+          is.na(Lifeform) ~ "U" # unknown
+        )
+      ),
+      abund = Cover,
+      abund_type = "cover"
+    ) %>%
+    select(site, year, plot, treat, species, guild, abund, abund_type)
+
+  return(scide_tbl)
 }
