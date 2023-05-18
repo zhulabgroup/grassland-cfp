be4dc24c30afbfe9cce6f3e9c14ac8f8a209b892
Kai Zhu <zhukai@umich.edu>
2023-05-18 08:44:41 -0400

    Add download_cfp()

---
+++ R/download.R
@@ -0,0 +1,13 @@
+download_cfp <- function() {
+  # Biodiversity Hotspots (version 2016.1)
+  # Michael Hoffman;  Kellee Koenig; Gill Bunting;  Jennifer Costanza;  Williams, Kristen J.
+  # https://doi.org/10.5281/zenodo.3261807
+  download.file(
+    url = "https://zenodo.org/record/3261807/files/hotspots_2016_1.zip?download=1",
+    destfile = "input/basemap/cfp.zip"
+  )
+  unzip(
+    zipfile = "input/basemap/cfp.zip",
+    exdir = "input/basemap/cfp"
+  )
+}
