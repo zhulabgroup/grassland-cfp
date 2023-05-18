d14b51ef51bd8580ee46315d5654f0bccd512216
Kai Zhu <zhukai@umich.edu>
2023-05-18 08:31:14 -0400

    Update setup_dir()

--- R/setup.R
+++ R/setup.R
@@ -3,34 +3,35 @@ setup_dir <- function(release = FALSE) {
 
   # setup base dir
   if (!release) { # create link to turbo
-    base_dir <- "~/.turbo/grassland/submission-science/"
-    link_create(str_c(base_dir, "input/"), "input")
-    link_create(str_c(base_dir, "intermediate/"), "intermediate")
-    link_create(str_c(base_dir, "output/"), "output")
+    base_dir <- "~/.turbo/grassland/submission/"
+    link_create(str_c(base_dir, "input"), "input")
+    link_create(str_c(base_dir, "intermediate"), "intermediate")
+    link_create(str_c(base_dir, "output"), "output")
   } else { # create dir with files
     NULL
   }
 
   # setup input dir
   dir_create(c(
-    "input/basemap/",
-    "input/biogeography/",
-    "input/climate/",
-    "input/community/"
+    "input/basemap",
+    "input/biogeography",
+    "input/climate",
+    "input/community"
   ))
 
   # setup intermediate dir
   dir_create(c(
-    "intermediate/"
+    "intermediate/background",
+    "intermediate/climate-niche",
+    "intermediate/observation-experiment",
+    "intermediate/synthesis-decomposition"
   ))
 
   # setup output dir
   dir_create(c(
-    "output/data/",
-    "output/figures/",
-    "output/figures/main/",
-    "output/figures/supp/",
-    "output/tables/"
+    "output/data",
+    "output/figures",
+    "output/tables"
   ))
 
   detach(package:fs, unload = TRUE)
