ea32b41455d3fcb63e9dceec3ad84d85c38b6e1f
Kai Zhu <zhukai@umich.edu>
2023-05-18 09:38:12 -0400

    Add tidy East Bay

--- R/tidy.R
+++ R/tidy.R
@@ -4,6 +4,11 @@ tidy_all <- function(
   # observational sites
   tidy_angelo(in_dir) %>% write_csv(str_c(out_dir, "angelo.csv"))
   tidy_carrizo(in_dir) %>% write_csv(str_c(out_dir, "carrizo.csv"))
+
+  eastbay_tbl <- tidy_eastbay(in_dir)
+  parse_pleasantonridge(eastbay_tbl) %>% write_csv(str_c(out_dir, "pleasantonridge.csv"))
+  parse_sunol(eastbay_tbl) %>% write_csv(str_c(out_dir, "sunol.csv"))
+  parse_vascocaves(eastbay_tbl) %>% write_csv(str_c(out_dir, "vascocaves.csv"))
 }
 
 # observation -------------------------------------------------------------
@@ -110,4 +115,81 @@ tidy_carrizo <- function(base_dir) {
     arrange(site, year, plot, species)
 
   return(carrizo_tbl)
+}
+
+tidy_eastbay <- function(base_dir) {
+  # community data
+  com_tbl <- base_dir %>%
+    str_c("Dudney/EBRPD_2002thru2012_Dec2013_BRXX AVXX updated.csv") %>%
+    read_csv(col_types = "cicidc") %>%
+    rename(plot = plot.ID, species_code = species) %>%
+    group_by(site, year, plot, species_code) %>%
+    summarize(hits = n()) %>%
+    group_by(site, year, plot) %>%
+    mutate(tot_hits = sum(hits)) %>%
+    group_by(site, year, plot, species_code) %>%
+    summarize(abund = hits / tot_hits)
+
+  # species data
+  spp_tbl <- base_dir %>%
+    str_c("Dudney/_VegSpCodeAttributes_2010.xlsx") %>%
+    readxl::read_xlsx() %>%
+    mutate(
+      species_code = toupper(species),
+      guild = str_c(
+        case_when(
+          `native/exotic` == "e" ~ "E", # Exotic
+          `native/exotic` == "n" ~ "N", # Native
+          TRUE ~ "U" # else is Unknown
+        ),
+        case_when(
+          `annual/perennial` == "a" ~ "A", # Annual
+          `annual/perennial` == "p" ~ "P", # Perennial
+          TRUE ~ "U" # else is Unknown
+        ),
+        case_when(
+          `forb/grass` == "f" ~ "F", # Forb
+          `forb/grass` == "g" ~ "G", # Grass
+          `forb/grass` == "n" ~ "R", # Rush
+          `forb/grass` == "s" ~ "S", # Shurb
+          `forb/grass` == "t" ~ "T", # Tree
+          TRUE ~ "U" # else is Unknown
+        )
+      )
+    ) %>%
+    select(species_code, species = latin, guild)
+
+  # combine
+  eastbay_tbl <- com_tbl %>%
+    left_join(spp_tbl, by = "species_code") %>%
+    mutate(abund_type = "point_intercept") %>%
+    mutate(species = coalesce(species, species_code)) %>% # if species == NA, replace with species_code
+    filter(
+      !(species %in% c("bare", "COWPIE", "gopher", "litter", "mosses", "rock", "soil")),
+      !str_detect(species, "unknown"),
+      is.na(guild) | guild != "UUU"
+    ) %>%
+    select(site, year, plot, species, guild, abund, abund_type) %>%
+    arrange(site, year, plot, species)
+
+  return(eastbay_tbl)
+}
+
+parse_pleasantonridge <- function(eastbay) {
+  eastbay %>%
+    filter(site == "PR", plot %in% str_c("PR", 4:9)) %>%
+    mutate(site = "pleasantonridge")
+}
+
+parse_sunol <- function(eastbay) {
+  eastbay %>%
+    filter(site == "SU", plot %in% str_c("SU", 1:9)) %>%
+    mutate(site = "sunol")
+}
+
+parse_vascocaves <- function(eastbay) {
+  eastbay %>%
+    filter(site == "VC", plot %in% str_c("VC", 1:10)) %>%
+    filter(!(year == 2012 & plot %in% c("VC1", "VC8", "VC9"))) %>%
+    mutate(site = "vascocaves")
 }
