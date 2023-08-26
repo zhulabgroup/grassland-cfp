ff0d44d16cab40aa528e25caccd15f1135d80c28
yiluansong <yiluansong@gmail.com>
2023-08-26 18:23:46 -0400

    save suppl figs

--- R/save_figure.R
+++ R/save_figure.R
@@ -15,60 +15,88 @@ save_figure <- function(out, dir = "alldata/output/figures/") {
 }
 
 save_figure_param <- function(gg_name) {
-  if (str_detect(gg_name, "niche")) {
+  if (gg_name== "niche") {
     type <- "main"
     width <- 8
     height <- 8 * 1.618
     filename <- "fig-main-niche.png"
   }
 
-  if (str_detect(gg_name, "community_index_obs")) {
+  if (gg_name== "community_index_obs") {
     type <- "main"
     width <- 11
     height <- 11 * 1.5
     filename <- "fig-main-obs.png"
   }
 
-  if (str_detect(gg_name, "community_index_exp")) {
+  if (gg_name=="community_index_exp") {
     type <- "main"
     width <- 10
     height <- 6.18
     filename <- "fig-main-exp.png"
   }
 
-  if (str_detect(gg_name, "community_shift")) {
+  if (gg_name== "community_shift") {
     type <- "main"
     width <- 9
     height <- 9 * 1.618
     filename <- "fig-main-shift.png"
   }
 
-  if (str_detect(gg_name, "species_change")) {
+  if (gg_name== "species_change") {
     type <- "main"
     width <- 11
     height <- 11.5
     filename <- "fig-main-gainloss.png"
   }
 
-  if (str_detect(gg_name, "cfp_cc")) {
+  if (gg_name== "cfp_cc") {
     type <- "supp"
     width <- 9
     height <- 9 * .618
     filename <- "fig-supp-cfp-cc.png"
   }
 
-  if (str_detect(gg_name, "data_avail")) {
+  if (gg_name=="data_avail") {
     type <- "supp"
     width <- 10
     height <- 10 * .618
     filename <- "fig-supp-data-avail.png"
   }
 
-  if (str_detect(gg_name, "site_cc")) {
+  if (gg_name== "site_cc") {
     type <- "supp"
     width <- 11
     height <- 11 * 1.5
     filename <- "fig-supp-site-cc.png"
+  }
+
+  if (gg_name== "niche_cool") {
+    type <- "supp"
+    width <- 8
+    height <- 8 * .618
+    filename <- "fig-supp-niche-cool.png"
+  }
+
+  if (gg_name== "niche_warm") {
+    type <- "supp"
+    width <- 8
+    height <- 8 * .618
+    filename <- "fig-supp-niche-warm.png"
+  }
+
+  if (gg_name== "species_change_obs") {
+    type <- "supp"
+    width <- 10
+    height <- 8
+    filename <- "fig-supp-gainloss-obs.png"
+  }
+
+  if (gg_name== "species_change_exp") {
+    type <- "supp"
+    width <- 12
+    height <- 8
+    filename <- "fig-supp-gainloss-exp.png"
   }
 
   out <- list(type = type, width = width, height = height, filename = filename)
