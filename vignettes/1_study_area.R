021606daf0f50807878337636ff335eb8822033b
yiluansong <yiluansong@gmail.com>
2023-10-13 22:59:06 -0400

    vignettes

---
+++ vignettes/1_study_area.R
@@ -0,0 +1,11 @@
+## ---- include = FALSE---------------------------------------------------------
+knitr::opts_knit$set(root.dir = here::here()) # knit from project directory
+knitr::opts_chunk$set(
+  collapse = TRUE,
+  out.width = "100%",
+  cache = T,
+  comment = "#>",
+  fig.width = 8,
+  fig.height = 8
+)
+
