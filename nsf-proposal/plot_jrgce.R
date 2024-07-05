2e46b99c411492c32bf98840845c9ced99135ca5
Kai Zhu <zhukai@umich.edu>
2024-07-05 10:58:48 -0400

    Move data/ and _setup.R

--- nsf-proposal/plot_jrgce.R
+++ nsf-proposal/plot_jrgce.R
@@ -1,9 +1,9 @@
-library(tidyverse)
+source("_setup.R")
 
-jrgce_com <- read_rds("nsf-proposal/data/cfp_community.rds")$exp %>%
+jrgce_com <- read_rds("data/cfp_community.rds")$exp %>%
   filter(site == "jrgce") %>%
   select(year, plot, treat, species, guild, abund)
 
-jrgce_npp <- read_csv("nsf-proposal/data/jrgce_biomass.csv", col_types = "iicd") %>%
+jrgce_npp <- read_csv("data/jrgce_biomass.csv", col_types = "iicd") %>%
   pivot_wider(names_from = grp, values_from = bio) %>%
   select(year = yr, plot = plt, anpp = ANPP, bnpp = BNPP, npp = NPP)
